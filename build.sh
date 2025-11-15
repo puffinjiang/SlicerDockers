8810376cc1262e15a98b6253030e7bcc0a034c2c
puffinjiang <36837780+puffinjiang@users.noreply.github.com>
2025-11-15 23:50:43 +0800

    feat: upgrade the version to 5.10.0

--- build.sh
+++ build.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
 #VERSION="4.11.20200930"
-VERSION="5.0.3"
+#VERSION="5.0.3"
+VERSION="5.10.0"
 
 docker build --build-arg VERSION=${VERSION} -t stevepieper/base:$VERSION base
 docker build --build-arg VERSION=${VERSION} -t stevepieper/x11:$VERSION x11
 docker build --build-arg VERSION=${VERSION} -t stevepieper/slicer:$VERSION slicer
 
-SLICER_MORPH_EXTS="MarkupsToModel Auto3dgm SegmentEditorExtraEffects Sandbox SlicerIGT RawImageGuess SlicerDcm2nii SurfaceWrapSolidify SlicerMorph"
+#SLICER_MORPH_EXTS="MarkupsToModel Auto3dgm SegmentEditorExtraEffects Sandbox SlicerIGT RawImageGuess SlicerDcm2nii SurfaceWrapSolidify SlicerMorph"
+#
+#docker build \
+#  --build-arg VERSION=${VERSION} --build-arg SLICER_EXTS="${SLICER_MORPH_EXTS}" \
+#  -t stevepieper/slicer-morph:$VERSION slicer-plus
+#
+#SLICER_DMRI_EXTS="UKFTractography SlicerDMRI"
+#
+#docker build \
+#  --build-arg VERSION=${VERSION} --build-arg SLICER_EXTS="${SLICER_DMRI_EXTS}" \
+#  -t stevepieper/slicer-dmri:$VERSION slicer-plus
 
-docker build \
-  --build-arg VERSION=${VERSION} --build-arg SLICER_EXTS="${SLICER_MORPH_EXTS}" \
-  -t stevepieper/slicer-morph:$VERSION slicer-plus
-
-SLICER_DMRI_EXTS="UKFTractography SlicerDMRI"
-
-docker build \
-  --build-arg VERSION=${VERSION} --build-arg SLICER_EXTS="${SLICER_DMRI_EXTS}" \
-  -t stevepieper/slicer-dmri:$VERSION slicer-plus
-
-docker build --build-arg VERSION=${VERSION} -t stevepieper/slicer-dev:$VERSION slicer-dev
-
-
-docker build -t stevepieper/slicer3:$VERSION slicer3
-docker build --build-arg VERSION=${VERSION} --no-cache -t stevepieper/slicer-chronicle:$VERSION slicer-chronicle
+#docker build --build-arg VERSION=${VERSION} -t stevepieper/slicer-dev:$VERSION slicer-dev
+#
+#
+#docker build -t stevepieper/slicer3:$VERSION slicer3
+#docker build --build-arg VERSION=${VERSION} --no-cache -t stevepieper/slicer-chronicle:$VERSION slicer-chronicle
 
